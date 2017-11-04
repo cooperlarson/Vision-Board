e35d0fc5b5eefd6b4f3675a2fb85a54f3b91f5f8
Cooper <cooper.larson1@gmail.com>
2017-11-03 20:49:31 -0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,79 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from './firebase';
-import ModalForm from './modal';
+import ContentHeader from './content-header';
+import GoalsList from './goals-list';
+import Header from './Header';
 
 class App extends Component {
-  //setting initial state
-  constructor() {
-    super();
-    this.state = {
-      title: '',
-      description: '',
-      imgUrl: '',
-      due: '',
-      goals: []
-    }
-  };
-
-  // retrieving goals from database
-  componentDidMount() {
-    const goalsRef = firebase.database().ref('goals');
-    goalsRef.on('value', (snapshot) => {
-      let goals = snapshot.val();
-      let newState = [];
-      for (let goal in goals) {
-        newState.push({
-          id: goal,
-          title: goals[goal].title,
-          description: goals[goal].description,
-          imgUrl: goals[goal].imgUrl,
-          due: goals[goal].due
-        });
-      }
-      this.setState({
-        goals: newState
-      });
-    });
-  }
-
-  removeItem(itemId) {
-    const goalsRef = firebase.database().ref(`/goals/${itemId}`);
-    goalsRef.remove();
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Vision Board</h1>
-        </header>
+        <Header />
         <div className="container-fluid">
-        <section className="content-header">
-          <div className="col-xs-4" />
-          <div className="col-xs-4">
-            <h3>All Goals</h3>
-          </div>
-          <div className="col-xs-4">
-            <ModalForm />
-          </div>
-        </section>
-        <section className='goals'>
-              {this.state.goals.map((goal) => {
-                const bgImg = {backgroundImage: `url(${goal.imgUrl})`}
-                return (
-                  <div key={goal.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3 goal-box">
-                    <div className="goal-item" style={bgImg}>
-                      <div className="goal-info">
-                        <i class="fa fa-times-circle-o delete-goal" aria-hidden="true" onClick={() => this.removeItem(goal.id)}></i>
-                        <h3>{goal.title}</h3>
-                        <p>Due: {goal.due}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-        </section>
+          <ContentHeader />
+          <GoalsList />
         </div>
       </div>
     );
---
+++ src/content-header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ModalForm from './modal';
+
+class ContentHeader extends Component {
+  render() {
+    return (
+      <section className="content-header">
+      <div className="col-xs-4" />
+      <div className="col-xs-4">
+        <h3>All Goals</h3>
+      </div>
+      <div className="col-xs-4">
+        <ModalForm />
+      </div>
+    </section>
+    );
+  }
+}
+
+export default ContentHeader;
